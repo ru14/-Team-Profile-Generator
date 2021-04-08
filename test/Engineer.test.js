218c0790fdad5c96f7ebbb801e5e1dd0b82a0845
ru14 <74753345+ru14@users.noreply.github.com>
2021-04-07 20:44:18 -0700

    error fixed

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -1,1 +1,7 @@
-// 
+const Engineer = require("../lib/Engineer");
+
+Test("should put GitHub account via constructor",() => {
+const testValue = "GithubUser";
+const e = new Engineer("Susu", 1, "test@test.com", testValue);
+expect(e.github).tobe(testValue);
+})
