1d5e27c48de333c6804595db243db579c5954617
ru14 <74753345+ru14@users.noreply.github.com>
2021-04-11 17:05:38 -0700

    error4 fixed

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
-const Employee = require("./lib/Employee");
 const Intern = require("./lib/Intern");
 const path = require("path");
 const fs = require("fs");
